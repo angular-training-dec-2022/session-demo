9b5458267b25b19c7ec02c3ffb9593de427cc8a8
Poonga <bpoongs1982@gmail.com>
2022-12-17 13:07:05 -0500

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { LifecycleParentComponent } from './angular-lifecycle/lifecycle-parent/lifecycle-parent.component';
+import { LifecycleChildComponent } from './angular-lifecycle/lifecycle-parent/lifecycle-child/lifecycle-child.component';
+import { BindingDemoComponent } from './binding-demo/binding-demo.component';
+import { FormsModule } from '@angular/forms';
+import { ColorListComponent } from './color-list/color-list.component';
+import { ColorItemComponent } from './color-list/color-item/color-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    LifecycleParentComponent,
+    LifecycleChildComponent,
+    BindingDemoComponent,
+    ColorListComponent,
+    ColorItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
