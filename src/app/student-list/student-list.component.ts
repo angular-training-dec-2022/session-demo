76dff623ff30e7e67e852789b4e0026bcda0e34c
Poonga <bpoongs1982@gmail.com>
2022-12-25 10:01:11 -0500

    services

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../services/student.service';
 import { Student } from './student';
 
 @Component({
@@ -10,60 +11,51 @@ export class StudentListComponent implements OnInit {
 
   filterData: string = '';
 
-  
-  allStudents: Student[] = [
-    {
-      id: 101,
-      studentName: 'Ram Mohan',
-      studentTotalMarks: 358,
-      studentDOB: new Date(5,5,1990),
-      studentGender: 'Male'
-    },
-    {
-      id: 102,
-      studentName: 'Nithya Mahadevan',
-      studentTotalMarks: 240,
-      studentDOB: new Date(7,7,1992),
-      studentGender: 'Female'
-    },
-    {
-      id: 103,
-      studentName: 'Rakesh Roy',
-      studentTotalMarks: 427,
-      studentDOB: new Date(2,2,1991),
-      studentGender: 'Male'
-    },
-    {
-      id: 104,
-      studentName: 'Meena Kumari',
-      studentTotalMarks: 353,
-      studentDOB: new Date(7,7,1992),
-      studentGender: 'Female'
-    }
-  ];
+  allStudents: Student[] = [];
 
   filterStudents: Student[] =this.allStudents;
 
-  constructor() { }
+  //studentService: StudentService = new StudentService(); // this line is not correct here because we as developers should not create the service object
+                                                            // let this work be done by angular
+
+  // studentService: StudentService;  //commented this because the injected student service is made private
+
+  constructor(private studentService: StudentService) {
+    //this.studentService = studentService; //commented this because the injected student service is made private
+  }
 
   ngOnInit(): void {
+    this.allStudents = this.studentService.fetchAllStudents();
+    this.filterStudents = this.allStudents;
   }
 
   addStudent(){
-    this.allStudents.push({
-      id: 105,
-      studentName: 'TEST',
-      studentTotalMarks: 343,
-      studentDOB: new Date(3,5,1992),
-      studentGender: 'Female'
-  });
+    // commented these lines because the new student should be added to the array in the servce class
+  //   this.allStudents.push({
+  //     id: 105,
+  //     studentName: 'TEST',
+  //     studentTotalMarks: 343,
+  //     studentDOB: new Date(3,5,1992),
+  //     studentGender: 'Female'
+  // });
+   let newStudent: Student =  {
+        id: 105,
+        studentName: 'TEST',
+        studentTotalMarks: 343,
+        studentDOB: new Date(3,5,1992),
+        studentGender: 'Female'
+    }
+
+    this.studentService.addStudent(newStudent);
+    this.allStudents = this.studentService.fetchAllStudents();
+    
   }
 
   filterByGender(){
     if(this.filterData == ''){
       this.filterStudents = this.allStudents
     }else{
-      this.filterStudents = this.allStudents.filter((eachStudent)=> eachStudent.studentGender.toLowerCase() == this.filterData.toLowerCase());
+      this.filterStudents = this.allStudents.filter((eachStudent)=> eachStudent.studentGender.toLowerCase().includes(this.filterData.toLowerCase()));
     }
       
     
